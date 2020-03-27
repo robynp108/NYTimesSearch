d5c7fc617fc05b006d450d3c45771177a16abc43
robynp108 <robynp108@gmail.com>
2020-03-27 13:17:04 -0500

    first back end test

---
+++ script.js
@@ -0,0 +1,12 @@
+var article = "covid19"
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + article + "&api-key=3mARzyoBMW0DZaaKZrZl1EQJcKsoYShT"
+
+//var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=covid19&api-key=3mARzyoBMW0DZaaKZrZl1EQJcKsoYShT";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
+
